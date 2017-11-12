ee99e159e14481d47fd913a8c7f860fc864f4417
santhim5 <31152790+santhim5@users.noreply.github.com>
2017-11-12 19:42:49 +0530

    Update JavaSample.java
    IOT-1 Javasample code checkin

--- JavaSample.java
+++ JavaSample.java
@@ -1,12 +1,9 @@
 import java.util.*;
- 
-public class AddTwoNum{
- 
-     public static void main(String []args){
-         int a,b,add;
-                 
-         add= 3+4;
-          
-         System.out.println("Addition is: " + add);
-     }
+public class AddTwoNum{ 
+  public static void main(String []args)
+  {
+    int a,b,add;
+    add= 3+4;
+    System.out.println("Addition is: " + add);
+  }
 }
